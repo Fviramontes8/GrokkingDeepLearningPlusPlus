5d43bd600dd49592f98969317da856412f0c6789
Francisco Viramontes <fviramontes8@unm.edu>
2022-07-20 17:17:51 -0600

    Update app/app.cpp
    Started implementing a multiple input and multiple output neural network
    with fixed weights.
    
    It currently satisfies the example in Chap3: Predicting with multiple
    inputs and outputs.

--- matrix_nn/app/app.cpp
+++ matrix_nn/app/app.cpp
@@ -1,15 +1,64 @@
 #include <iostream>
+#include <vector>
 
 #include "../include/matrix/matrix.hpp"
 
+using fvector = std::vector<float>;
+
+// Chapter 3, Predicting with multiple inputs and outputs
+fvector neural_network(
+		const fvector& input, 
+		const FENK::Matrix<float>& weights
+	) {
+	fvector pred = weights * input;
+
+	return pred;
+}
+
 int main() {
-	FENK::Matrix<int> m = { 
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}
+	FENK::Matrix<float> weights = { 
+		{0.1, 0.1, -0.3},
+		{0.1, 0.2, 0.0 },
+		{0.0, 1.3, 0.1}
 	};
 
-	std::cout << m << '\n';
+	std::cout << weights << '\n';
+
+	fvector toes = {8.5, 9.5, 9.9, 9.0};
+	fvector win_loss = {0.65, 0.8, 0.8, 0.9};
+	fvector nfans = {1.2, 1.3, 0.5, 1.0};
+
+	FENK::Matrix<float> input = {
+		toes,
+		win_loss,
+		nfans
+	};
+
+	for (std::size_t i=0; i<toes.size(); ++i) {
+		fvector result = neural_network(
+			{input[0][i], input[1][i], input[2][i]},
+			weights
+		);
+		for (const auto& x: result) {
+			std::cout << x << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	FENK::Matrix<int> a = {
+		{1, 2, 3},
+		{4, 5, 6}
+	};
+
+	FENK::Matrix<int> b = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12}
+	};
+
+	FENK::Matrix<int> c = a * b;
+	
+	std::cout << '\n' << c << '\n';
 
 	return 0;
 }
