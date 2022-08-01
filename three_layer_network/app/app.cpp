ee5a17c4b372a5d26c676c4a3b3bcbe0f9cbd92a
Francisco Viramontes <fviramontes8@unm.edu>
2022-08-01 15:19:44 -0600

    Update app/app
    Added book example of a three layer nerual network

--- three_layer_network/app/app.cpp
+++ three_layer_network/app/app.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
+#include <vector>
 
 #include "../include/matrix/matrix.hpp"
 
+FENK::Matrix2D<float> neural_network(
+		const FENK::Matrix2D<float>& input, 
+		const std::vector<FENK::Matrix2D<float>>& weights) {
+	FENK::Matrix2D<float> hidden_result = weights[0] * input;
+	return weights[1] * hidden_result;
+}
+
+// Prediciting on predictions
 int main() {
-	FENK::Matrix2D<int> x(2, 2, 1);
-
-	FENK::Matrix2D<int> y(2, 2, 7);
-
-	std::cout << x << '\n';
-	std::cout << y << '\n';
-
-	FENK::Matrix2D z = {
-		{1, 2, 3},
-		{4, 5, 6}
+	FENK::Matrix2D<float> input_data = {
+		{8.5, 9.5, 9.9, 9.0},
+		{0.65, 0.8, 0.8, 0.9},
+		{1.2, 1.3, 0.5, 1.0}
 	};
 
-	std::cout << z << '\n';
+	FENK::Matrix2D<float> input_weights = {
+		{0.1, 0.2, -0.1},
+		{-0.1, 0.1, 0.9},
+		{0.1, 0.4, 0.1}
+	};
+
+	FENK::Matrix2D<float> hidden_layer_weights = {
+		{0.3, 1.1, -0.3},
+		{0.1, 0.2, 0.0},
+		{0.0, 1.3, 0.1}
+	};
+
+	std::vector<FENK::Matrix2D<float>> weights = {
+		input_weights,
+		hidden_layer_weights
+	};
+
+	FENK::Matrix2D<float> pred = neural_network(input_data, weights);
+	std::cout << pred << '\n';
 
 	return 0;
 }
